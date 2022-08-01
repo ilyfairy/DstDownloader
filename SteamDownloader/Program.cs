45f167b8aaaddb971a0ff04f107d54307230ab76
小仙女 <me@ilyfairy.com>
2022-08-01 10:45:39 +0800

    提交

--- SteamDownloader/Program.cs
+++ SteamDownloader/Program.cs
@@ -16,46 +16,8 @@ namespace DepotDownloader
 
     class Program
     {
-        static string MakeProgressStr(float progress, int length = 12, char start = '[', char end = ']', char elapsed = '#', char whitespace = ' ')
-        {
-            int elapsedChars = (int)((length - 2) * progress);
-            int whiteChars = (int)((length - 2) - elapsedChars);
-
-            return $"{start}{new string(elapsed, elapsedChars)}{new string(whitespace, whiteChars)}{end}";
-        }
         static async Task Main(string[] args)
         {
-            //{
-
-            //    var client = new HttpClient();
-
-            //    var msg = await client.SendAsync(
-            //        new HttpRequestMessage(HttpMethod.Get, "https://drive.233.pink/Video/%E3%80%90%E4%B8%AD%E6%96%87%E7%89%88%E3%80%91%E5%A4%A7%E4%BE%A6%E6%8E%A2%E7%9A%AE%E5%8D%A1%E4%B8%98%EF%BC%882019%EF%BC%89.mp4"),
-            //        HttpCompletionOption.ResponseHeadersRead);
-
-            //    Console.ReadLine();
-            //    long? length = msg.Content.Headers.ContentLength;
-            //    byte[] buffer = new byte[1024];
-            //    var stream = await msg.Content.ReadAsStreamAsync();
-            //    var ms = new MemoryStream();
-            //    var cpTask = stream.CopyToAsync(ms);
-            //    await Task.Run(async () =>
-            //    {
-            //        while (!cpTask.IsCompleted)
-            //        {
-            //            await Task.Delay(100);
-
-            //            float? progress = (float)ms.Position / length;
-            //            Console.WriteLine($"{MakeProgressStr(progress ?? 1f, 32)} {(progress.HasValue ? progress?.ToString("P") : "(Progress unsupported)")}");
-            //        }
-            //    });
-            //    return;
-            //}
-
-
-            //MainAsync(args).GetAwaiter().GetResult();
-
-
             //args = new string[] { "-app", "322330", "-pubfile", "791838548" };
             //args = new string[] { "-app", "343050" };
 
